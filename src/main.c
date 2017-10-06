fbf5742f6b2e22f9fafe17163c5bbdc04f7d0080
nyuno <nyunohyuno@gmail.com>
2017-10-07 03:11:17 +0900

    main.c PR 적용 걍 커밋으로 올림

--- src/main.c
+++ src/main.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
 
 #include "commands.h"
 #include "utils.h"
 
-extern char g_current_directory[1024];
-
-static void release_argv(int argc, char** argv);
+static void release_argv(int argc, char*** argv);
 
 int main()
 {
@@ -16,37 +13,41 @@ int main()
   int argc;
   char** argv;
 
-  getcwd(g_current_directory, sizeof(g_current_directory));
-
   while (1) {
     fgets(buf, 8096, stdin);
+
     mysh_parse_command(buf, &argc, &argv);
 
-    if (strcmp(buf, "") == 0) {
+    if (strcmp(argv[0], "") == 0) {
       goto release_and_continue;
     } else if (strcmp(argv[0], "cd") == 0) {
-      do_cd(argc, argv);
+      if (do_cd(argc, argv)) {
+        fprintf(stderr, "cd: Invalid arguments\n");
+      }
     } else if (strcmp(argv[0], "pwd") == 0) {
-      do_pwd(argc, argv);
+      if (do_pwd(argc, argv)) {
+        fprintf(stderr, "pwd: Invalid arguments\n");
+      }
     } else if (strcmp(argv[0], "exit") == 0) {
-      goto release_and_continue;
+      goto release_and_exit;
     } else {
       fprintf(stderr, "%s: command not found\n", argv[0]);
     }
 release_and_continue:
-    release_argv(argc, argv);
+    release_argv(argc, &argv);
     continue;
 release_and_exit:
-    release_argv(argc, argv);
+    release_argv(argc, &argv);
     break;
   }
 
   return 0;
 }
 
-static void release_argv(int argc, char** argv) {
+static void release_argv(int argc, char*** argv) {
   for (int i = 0; i < argc; ++i) {
-    free(argv[i]);
+    free((*argv)[i]);
   }
-  free(argv);
+  free(*argv);
+  *argv = NULL;
 }
